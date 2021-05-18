9775137d04c79163823f59f9ecfa14a1e3123ef3
potykion <potykion@gmail.com>
2021-05-18 17:53:47 +0300

    YT shuffle

---
+++ README.md
@@ -0,0 +1,35 @@
+# Рандомайзер Ютуб видосов
+
+Предыстория тут: https://potykion.github.io/dev/ideas/
+
+Скрипт получает все видики из плейлиста и рандомизирует их прямо в плейлисте
+
+## Сетап
+
+1. Создаем проект в Google Cloud Platform
+2. Включаем Youtube Data API в https://console.cloud.google.com/apis/library
+3. Переходим в Credentials + создаем OAuth client ID с типом Desktop
+4. Качаем client_secret.json
+
+   ![client secret download](img.png)
+
+5. Закидываем в папку с проектом, создаем `.env`-файл с названием client_secret.json файла:
+
+   ```
+   CLIENT_SECRET_JSON=client_secret_843289797199-ac7kigb3b8qv3rmu3eis7rr93dfl0k4u.apps.googleusercontent.com.json
+   ```
+
+6. Ставим зависимости: `poetry install`
+
+7. Запускаем скрипт: `python main.py`
+
+8. При запуске будет предожено перейти по гугл-ссылке, там может быть такое:
+
+   ![takoe](img_1.png)
+
+   В таком случаем прожимаем `Advanced > Go to project...`
+
+9. Все принимаем, в конце будет предложено скопировать код, типа `4/1AY0e-g5MsByqd2EJ6fCg...` - его вставляем
+   в `Enter the authorization code:` из пункта 7
+
+10. Скрипт должен начать грузить инфу о видиках и рандомизировать их
---
+++ main.py
@@ -0,0 +1,86 @@
+import os
+import random
+
+from dotenv import load_dotenv
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+
+def main():
+    load_dotenv()
+    youtube = setup_youtube()
+    playlist_items = get_all_playlist_items(youtube)
+    reorder_playlist_items(youtube, playlist_items)
+
+
+def setup_youtube():
+    # https://developers.google.com/youtube/v3/docs/playlistItems/update?apix=true
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        os.environ["CLIENT_SECRET_JSON"],
+        ["https://www.googleapis.com/auth/youtube.force-ssl"]
+    )
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        "youtube",
+        "v3",
+        credentials=credentials
+    )
+    return youtube
+
+
+def get_all_playlist_items(youtube):
+    # https://www.youtube.com/playlist?list=PLdb8DVmvU9i5bGINNz10f-ga_bqD41O4q
+    playlist_id = "PLdb8DVmvU9i5bGINNz10f-ga_bqD41O4q"
+
+    playlist_items = []
+    cursor = None
+    limit = 50
+
+    while True:
+        # https://developers.google.com/youtube/v3/docs/playlistItems/list
+        list_resp = (
+            youtube.playlistItems()
+                .list(
+                part="snippet",
+                playlistId=playlist_id,
+                maxResults=limit,
+                pageToken=cursor
+            )
+                .execute()
+        )
+
+        new_items = list_resp["items"]
+        playlist_items.extend(new_items)
+        print(len(playlist_items))
+
+        if len(new_items) < limit:
+            break
+        else:
+            cursor = list_resp["nextPageToken"]
+
+    return playlist_items
+
+
+def reorder_playlist_items(youtube, playlist_items):
+    random.shuffle(playlist_items)
+
+    for index, playlist_item in enumerate(playlist_items):
+        print(f"{index + 1} / {len(playlist_items)}")
+        # https://developers.google.com/youtube/v3/docs/playlistItems/update
+        update_resp = youtube.playlistItems().update(
+            part="snippet",
+            body={
+                "id": playlist_item["id"],
+                "snippet": {
+                    "playlistId": playlist_item["snippet"]["playlistId"],
+                    "position": random.randint(0, len(playlist_items)),
+                    "resourceId": playlist_item["snippet"]["resourceId"]
+                }
+            }
+        ).execute()
+
+
+if __name__ == '__main__':
+    main()
